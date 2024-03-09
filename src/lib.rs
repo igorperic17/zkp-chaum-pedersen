b37b145651f1e1ae7f515db878825db9da51f504
Igor Peric <igorperic@live.com>
2024-03-09 17:12:32 +0100

    added ZKP struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,38 @@
 use num_bigint::{BigUint, RandBigInt};
-use rand;
 
-// g^x mod p
-pub fn exponentiate(n: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
-    n.modpow(exponent, modulus)
+pub struct ZKP {
+    p: BigUint,
+    q: BigUint,
+    g: BigUint,
+    h: BigUint
 }
 
-// output => s = k - c * x mod q
-pub fn solve(k: &BigUint, c: &BigUint, x: &BigUint, q: &BigUint) -> BigUint {
-    if *k >= c * x {
-        return (k - c * x).modpow(&BigUint::from(1u32), q);
+impl ZKP {
+    // g^x mod p
+    pub fn exponentiate(n: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
+        n.modpow(exponent, modulus)
     }
-    return q - (c * x - k).modpow(&BigUint::from(1u32), q);
-}
 
-// cond1: r1 = g^s * y1 * c
-// cond2: r2 = h^s * y2 * c
-pub fn verify(r1: &BigUint, r2: &BigUint, y1: &BigUint, y2: &BigUint, g: &BigUint, h: &BigUint, c: &BigUint, s: &BigUint, p: &BigUint) -> bool {
-    let cond1 = *r1 == (g.modpow(s, p) * y1.modpow(c, p)).modpow(&BigUint::from(1u32), p);
-    let cond2 = *r2 == (h.modpow(s, p) * y2.modpow(c, p)).modpow(&BigUint::from(1u32), p);
-    cond1 && cond2
-}
+    // output => s = k - c * x mod q
+    pub fn solve(&self, k: &BigUint, c: &BigUint, x: &BigUint) -> BigUint {
+        if *k >= c * x {
+            return (k - c * x).modpow(&BigUint::from(1u32), &self.q);
+        }
+        return &self.q - (c * x - k).modpow(&BigUint::from(1u32), &self.q);
+    }
 
-pub fn generate_random_below(bound: &BigUint) -> BigUint {
-    let mut rng = rand::thread_rng();
-    rng.gen_biguint_below(bound)
+    // cond1: r1 = g^s * y1 * c
+    // cond2: r2 = h^s * y2 * c
+    pub fn verify(&self, r1: &BigUint, r2: &BigUint, y1: &BigUint, y2: &BigUint, c: &BigUint, s: &BigUint) -> bool {
+        let cond1 = *r1 == (&self.g.modpow(s, &self.p) * y1.modpow(c, &self.p)).modpow(&BigUint::from(1u32), &self.p);
+        let cond2 = *r2 == (&self.h.modpow(s, &self.p) * y2.modpow(c, &self.p)).modpow(&BigUint::from(1u32), &self.p);
+        cond1 && cond2
+    }
+
+    pub fn generate_random_below(bound: &BigUint) -> BigUint {
+        let mut rng = rand::thread_rng();
+        rng.gen_biguint_below(bound)
+    }
 }
 
 
@@ -39,32 +47,34 @@ mod test {
         let p = BigUint::from(23u32);
         let q = BigUint::from(11u32);
 
+        let zkp = ZKP { p: p.clone(), q: q.clone(), g: g.clone(), h: h.clone() };
+
         let x = BigUint::from(6u32);
         let k = BigUint::from(7u32);
 
         let c = BigUint::from(4u32);
 
 
-        let y1 = exponentiate(&g, &x, &p);
-        let y2 = exponentiate(&h, &x, &p);
+        let y1 = ZKP::exponentiate(&g, &x, &p);
+        let y2 = ZKP::exponentiate(&h, &x, &p);
         assert_eq!(y1, BigUint::from(2u32));
         assert_eq!(y2, BigUint::from(3u32));
 
-        let r1 = exponentiate(&g, &k, &p);
-        let r2 = exponentiate(&h, &k, &p);
+        let r1 = ZKP::exponentiate(&g, &k, &p);
+        let r2 = ZKP::exponentiate(&h, &k, &p);
         assert_eq!(r1, BigUint::from(8u32));
         assert_eq!(r2, BigUint::from(4u32));
 
-        let s = solve(&k, &c, &x, &q);
+        let s = zkp.solve(&k, &c, &x);
         assert_eq!(s, BigUint::from(5u32));
 
-        let result = verify(&r1, &r2, &y1, &y2, &g, &h, &c, &s, &p);
+        let result = zkp.verify(&r1, &r2, &y1, &y2, &c, &s);
         assert!(result);
 
         // fake secret
         let x_fake = BigUint::from(14u32);
-        let s_fake = solve(&k, &c, &x_fake, &q);
-        let result = verify(&r1, &r2, &y1, &y2, &g, &h, &c, &s_fake, &p);
+        let s_fake = zkp.solve(&k, &c, &x_fake);
+        let result = zkp.verify(&r1, &r2, &y1, &y2, &c, &s_fake);
         assert!(!result);
 
     }
@@ -77,23 +87,24 @@ mod test {
         let p = BigUint::from(23u32);
         let q = BigUint::from(11u32);
 
+        let zkp = ZKP { p: p.clone(), q: q.clone(), g: g.clone(), h: h.clone() };
+
         let x = BigUint::from(6u32);
-        let k = generate_random_below(&q);
+        let k = ZKP::generate_random_below(&q);
 
-        let c = generate_random_below(&q);
+        let c = ZKP::generate_random_below(&q);
 
 
-        let y1 = exponentiate(&g, &x, &p);
-        let y2 = exponentiate(&h, &x, &p);
+        let y1 = ZKP::exponentiate(&g, &x, &p);
+        let y2 = ZKP::exponentiate(&h, &x, &p);
         assert_eq!(y1, BigUint::from(2u32));
         assert_eq!(y2, BigUint::from(3u32));
 
-        let r1 = exponentiate(&g, &k, &p);
-        let r2 = exponentiate(&h, &k, &p);
+        let r1 = ZKP::exponentiate(&g, &k, &p);
+        let r2 = ZKP::exponentiate(&h, &k, &p);
+        let s = zkp.solve(&k, &c, &x);
 
-        let s = solve(&k, &c, &x, &q);
-
-        let result = verify(&r1, &r2, &y1, &y2, &g, &h, &c, &s, &p);
+        let result = zkp.verify(&r1, &r2, &y1, &y2, &c, &s);
         assert!(result);
     }
 }
